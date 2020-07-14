d117480ce0b55735cfe8d3b72e1310c0a6fcb025
VigneshR <67826945+Cronos07@users.noreply.github.com>
2020-07-15 01:39:31 +0530

    Add files via upload

---
+++ day9/src/delete.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+
+function Todo() {
+    const [todos, setTodos] = useState([]);
+    const [text, setText] = useState("");
+    const [error, setError] = useState(false);
+
+    const add = () => {
+        if (text !== "") {
+            todos.push(text);
+            localStorage.setItem("todos",JSON.stringify(todos));
+            setTodos([...todos]);
+            setText("");
+            setError(false);
+        } else setError(true);
+    };
+
+
+    useEffect(()=>{
+        const todos = localStorage.getItem("todos");
+        if(todos) setTodos(JSON.parse(todos))
+    },[])
+
+    const remove = (id)=>{
+        todos.splice(id,1);
+        localStorage.setItem("todos",JSON.stringify(todos));
+        setTodos([...todos]);
+    }
+
+    return (
+        <div>
+            <h1 className="heading">TODO</h1>
+            <div className="textbox">
+                <input
+                    type="text"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button className="add" onClick={add}>
+                    Add
+                </button>
+                {error && (
+                    <div style={{ color: "red" }}>
+                        Please Enter Content of Todo
+                    </div>
+                )}
+            </div>
+            <div className="container">
+                {todos.map((todo, index) => (
+                    <div className="todo" key={index} onClick={()=>remove(index)}>
+                        {todo}
+                        <div className="delete">x</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
